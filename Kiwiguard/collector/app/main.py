3a348609f214156be385b229a01bc3ee0c3a9196
Max <editing4life@gmail.com>
2024-10-09 20:22:52 -0500

    Removed unused deps

--- Kiwiguard/collector/app/main.py
+++ Kiwiguard/collector/app/main.py
@@ -1,20 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 import logging
-
-class Metrics(BaseModel):
-    cpu_percent: float
-    memory_percent: float
-    memory_free: float
-    memory_total: float
-    disk_percent: float
-    disk_free: float
-    disk_total: float
-    timestamp: float
+from models import Metrics
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
-
 
 @app.post('/metrics/')
 async def metrics(metrics: Metrics):
